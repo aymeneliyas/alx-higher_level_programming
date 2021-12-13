0b50e08d9a3a7a89247448fb66b0e8908cfaf073
aymen eliyas <aymeneliyas@gmail.com>
2021-12-13 08:57:37 -0800

    worked on import modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,8 +4,7 @@ from sys import argv
 i, res = 1, 0
 
 if __name__ == '__main__':
-     while i < len(argv):
-         res += int(argv[i])
-         i += 1
-     print(res)
-
+    while i < len(argv):
+        res += int(argv[i])
+        i += 1
+    print(res)
--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,49 +1,49 @@
 #!/usr/bin/python3
 def add(a, b):
-        """My addition function
+    """My addition function
 
-         Args:
-             a: first integer
-             b: second integer
+     Args:
+         a: first integer
+         b: second integer
 
-         Returns:
-             The return value. a + b
-         """
-         return (a + b)
+     Returns:
+         The return value. a + b
+    """
+    return (a + b)
 
 
 def sub(a, b):
-         """My subtraction function
+    """My subtraction function
 
-         Args:
-             a: first integer
-             b: second integer
+    Args:
+    a: first integer
+    b: second integer
 
-         Returns:
-                                                                                               The return value. a - b
-                                                                                            """                                                                                return (a - b)
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
 
-                                                                                        def mul(a, b):
-                                                                                                """My multiplication function
+def mul(a, b):
+    """My multiplication function
 
-                                                                                                    Args:
-                                                                                                            a: first integer
-                                                                                                                    b: second integer
+    Args:
+        a: first integer
+        b: second integer
 
-                                                                                                                        Returns:
-                                                                                                                                The return value. a * b
-                                                                                                                                    """
-                                                                                                                                        return (a * b)
+        Returns:
+            The return value. a * b
+    """
+    return (a * b)
 
 
-                                                                                                                                    def div(a, b):
-                                                                                                                                            """My division function
+def div(a, b):
+    """My division function
 
-                                                                                                                                                Args:
-                                                                                                                                                        a: first integer
-                                                                                                                                                                b: second integer
-
-                                                                                                                                                                    Returns:
-                                                                                                                                                                            The return value. a / b
-                                                                                                                                                                                """
-                                                                                                                                                                                    return int(a / b)
+        Args:
+            a: first integer
+            b: second integer
+        Returns:
+            The return value. a / b
+    """
+    return int(a / b)
